3731781aee8cfb24ddee5537ea67da64aaa65159
Yanik Crepeau <ycrepeau@me.com>
2020-12-16 10:38:46 -0500

    Working project

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,62 +1,31 @@
-<template>
-  <div>
-    <Nuxt />
-  </div>
+<template lang="pug">
+.layout
+  header.header.
+    The header will be located here
+  .container
+    Nuxt
+
 </template>
 
-<style>
-html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+<script>
+export default {
+  mounted() {
+    console.log('default mounted')
+  },
 }
+</script>
 
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
+<style lang="scss">
+@import '@/assets/scss/app.scss';
+.header {
+  box-sizing: content-box;
+  color: $primary-color;
+  background-color: $secondary-color;
+  height: 24px;
+  padding: 10px;
 }
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.container {
+  margin: 0 auto;
+  width: 800px;
 }
 </style>
